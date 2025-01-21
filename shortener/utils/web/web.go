d5da4c9665da4af85748d6880716e0caff5dc696
Swastik Chaudhary <swastikkumar50@gmail.com>
2025-01-22 02:26:34 +0530

    Fixed Unmarshal JSON bug

--- shortener/utils/web/web.go
+++ shortener/utils/web/web.go
@@ -7,20 +7,16 @@ import (
 	"net/http"
 )
 
-// Unmarshals Request's Body into out variable
 func UnmarshalJSON(r *http.Request, out interface{}) error {
-	// Check if request is empty
 	if r.Body == nil {
 		return errors.New("body is empty")
 	}
 
-	// Read Body
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		return err
 	}
 
-	// Unmarshal JSON
 	err = json.Unmarshal(body, out)
 	if err != nil {
 		return err
