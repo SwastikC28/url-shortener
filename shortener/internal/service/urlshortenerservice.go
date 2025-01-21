f04c24937f05f99b735e55f6aeb6c1899319a55e
Swastik Chaudhary <swastikkumar50@gmail.com>
2025-01-22 02:08:02 +0530

    Added service

---
+++ shortener/internal/service/urlshortenerservice.go
@@ -0,0 +1,71 @@
+package service
+
+import (
+	"math/rand"
+	"time"
+
+	"url-shortener/internal/command"
+	"url-shortener/internal/model"
+	"url-shortener/internal/repository"
+)
+
+type URLShortenerService struct {
+	repo *repository.URLShortenerRepository
+}
+
+func NewURLShortenerService(repo *repository.URLShortenerRepository) *URLShortenerService {
+	return &URLShortenerService{repo: repo}
+}
+
+func (service *URLShortenerService) ShortenURL(cmd *command.CreateShortenURLCommand) (string, error) {
+	alias := cmd.LongURL
+	if cmd.CustomAlias != "" {
+		alias = cmd.CustomAlias
+	}
+
+	shortURL := service.shortenURL()
+
+	data := model.URLData{
+		ShortURL:         shortURL,
+		LongURL:          alias,
+		AccessCount:      0,
+		AccessTimestamps: make([]string, 0),
+	}
+
+	service.repo.AddNewURL(&data)
+
+	return data.ShortURL, nil
+}
+
+func (service *URLShortenerService) DecodeShortURL(alias string) (string, error) {
+	urlData, err := service.repo.GetURL(alias)
+	if err != nil {
+		return "", err
+	}
+
+	return urlData.LongURL, nil
+}
+
+func (service *URLShortenerService) UpdateShortURL(cmd *command.UpdateShortURLCommand) error {
+	return nil
+}
+
+func (service *URLShortenerService) GetAnalytics(alias string) error {
+	return nil
+}
+
+func (service *URLShortenerService) shortenURL() string {
+	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+	var shortURL [6]byte
+
+	// Seed the random number generator to ensure randomness
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 0; i < 6; i++ {
+		randomNumber := rand.Intn(len(charset))
+		shortURL[i] = charset[randomNumber]
+	}
+
+	return string(shortURL[:])
+}
