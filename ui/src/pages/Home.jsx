f8330caf3cd2c6ab46e2a82b7581338134b6bb8c
Swastik Chaudhary <swastikkumar50@gmail.com>
2025-02-08 00:59:53 +0530

    Added HTTP Req when submitting

--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -1,32 +1,65 @@
-import React, { useState } from 'react';
-import { Button, Container, Form, InputGroup } from 'react-bootstrap';
+import React, { use, useState } from 'react';
+import { Button, Col, Container, Form, InputGroup } from 'react-bootstrap';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import axios from 'axios'
 
 const Home = () => {
 
     const [url, setURL] = useState('')
+    const [alias, setAlias] = useState('')
+    const [ttl, setTTL] = useState(120)
 
-    const submitHandler = (e) => {
+    const [shortURL, setShortURL] = useState('')
+
+    const submitHandler = async (e) => {
         e.preventDefault()
 
-        console.log(url);
-        
+        try {
+            const response = await axios.post("http://localhost:8080/shorten", {
+                "long_urls": url,
+                "custom_alias": "",
+                "ttl_seconds": 120,
+            })
+
+            console.log(response.data);
+            
+            // const data=response.data
+            // if data
+        } catch (error) {
+
+        }
     }
 
-    const onChangeHandler = (e) => {
+    const onURLChangeHandler = (e) => {
         setURL(e.target.value)
+    }
+
+    const onAliasChangeHandler = (e) => {
+        setAlias(e.target.value)
+    }
+
+    const onTTLChangeHandler = (e) => {
+        setTTL(e.target.value)
     }
 
     return (
         <Container className='url-cont'>
             <h1 className='my-5'>URL Shortener</h1>
             <Form onSubmit={submitHandler}>
-                <InputGroup>
-                    <Form.Control type="text" placeholder="Enter the link here" value={url} onChange={onChangeHandler} />
-                    <Button variant="primary" type="submit">
-                        <ChevronRightIcon />
-                    </Button>
+                <InputGroup className='my-2'>
+                    <Form.Control type="text" placeholder="Enter the link here" value={url} onChange={onURLChangeHandler} />
                 </InputGroup>
+
+                <InputGroup className="my-2">
+                    <div className="d-flex gap-3 w-100">
+                        <Form.Control type="text" placeholder="Enter the custom alias" value={alias} onChange={onAliasChangeHandler} />
+                        <Form.Control type="number" placeholder="Enter the TTL in seconds" value={ttl} onChange={onTTLChangeHandler} />
+                    </div>
+                </InputGroup>
+
+                <Button variant="primary" type="submit" className='my-4'>
+                    Shorten <ChevronRightIcon />
+                </Button>
             </Form>
         </Container>
     );
